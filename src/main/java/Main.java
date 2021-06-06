5e877f7b6564d320a27cc621793297ba2f80ca2c
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 08:30:52 -0400

    V1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int[] num = new int[3];
 
+        System.out.print("Enter the first number. ");
+        num[0] = input.nextInt();
 
+        System.out.print("Enter the second number. ");
+        num[1] = input.nextInt();
+
+        System.out.print("Enter the third number. ");
+        num[2] = input.nextInt();
+
+        int largestNum = -1;
+        if((num[0] > num[1]) && (num[0] > num[2])) largestNum = 0;
+        else if((num[1] > num[0]) && (num[1] > num[2])) largestNum = 1;
+        else if((num[2] > num[0]) && (num[2] > num[1])) largestNum = 2;
+        else System.exit(largestNum);
+
+        System.out.printf("The largest number is %d.", num[largestNum]);
     }
 }
